63887639cab2af92e6ab9a5e9687d200400a37a5
Pierre-Gilles Levallois <pgl@erasme.org>
2015-02-11 18:30:51 +0100

    adds spash screen on lcd

--- arduino/brewFlowMeterInterrupts/brewFlowMeterInterrupts.ino
+++ arduino/brewFlowMeterInterrupts/brewFlowMeterInterrupts.ino
@@ -12,9 +12,10 @@
 // define DISABLE_PCINT_MULTI_SERVICE below to limit the handler to servicing a single interrupt per invocation.
 // #define       DISABLE_PCINT_MULTI_SERVICE
 //-------- define the above in your sketch, if applicable ------------------------------------------------------
-#include <math.h>
 #include <PinChangeInt.h>
 #include <LiquidCrystal.h>
+
+#define APP_VERSION "1.0"
 
 // Rotary encoder push action on Pin #3 for interrupt 1
 #define ENC_PUSH 3 //Label SW on encoder 
@@ -27,28 +28,149 @@
 #define LCD_B 9
 
 #define ENC_STEP  0.1
+// Application status
+// App is waiting for sensors or buttons changes : Valve is closed
+// displaying current passing volume, desired volume, total volume, flowrate
+// push button may open valve after APP_CONFIRM mode
+#define APP_WAITING 0
+// App is running water thru valve : valve is opened
+// displaying current passing volume, desired volume, total volume, flowrate
+// push button may interrupt to close valve and return to APP_WAITING mode
+#define APP_RUNNING 1
+// App is in setting mode, valve is closed
+// displying 'setting mode' on first line and desired volume to adjust on second line
+// turing the rotary encoder adjust disered volume of water,
+// push button may set adjusted volume of water and return to APP_WAITING mode
+#define APP_SETTING 2
+// App is in confirmation mode, valve is closed
+// displaying a confirmation message on first line and  yes | no choice on second line.
+// push button may set desired volume and return to APP_WAITING mode
+#define APP_CONFIRM 3
+// App is in confirmation mode, valve is closed, 
+// displaying a confirmation message on first line and  [run] [set] [x]
+// push button may set desired volume and return to APP_RUNNING, APP_SETTING, or APP_WAITING mode
+#define APP_OPTIONS 4
+// Choices on Options screen
+#define CHOICE_RUNNING 1
+#define CHOICE_SETTING 2
+#define CHOICE_CANCEL  0
 
 // Liquid Crystal display on pins A0, A1, A2, A3, A4, A5
 LiquidCrystal lcd(14, 15, 16, 17, 18, 19);
 
 boolean debug_mode = true;
 
-//volatile unsigned 
+// encoder variables
 volatile int encoderPos = 0;
-//unsigned
 volatile int lastReportedPos = 1;
-
 boolean A_set = false;
 boolean B_set = false;
+int encoder_button_state = 0;
+
 //    uint8_t latest_interrupted_pin;
 //    uint8_t interrupt_count[20]={0}; // 20 possible arduino pins
 
+// LCD variables
+int lcd_brightness = 100;
+
 float app_target_liters = 0;
+
+// Application variables
+int app_status = APP_WAITING;
+int app_previous_status = APP_WAITING;
+int app_choice = CHOICE_CANCEL;
+
+/*************************************************
+ * Setter for application status
+ **************************************************/
+void app_set_state(int s) {
+  app_previous_status = app_get_state();
+  app_status = s;
+}
+
+/*************************************************
+ * Getter for application status
+ **************************************************/
+int app_get_state() {
+  return app_status;
+}
+
+/*************************************************
+ * Getter for application previous status
+ **************************************************/
+int app_get_previous_state() {
+  return app_previous_status;
+}
+
+/*************************************************
+ * set backlight color on lcd
+ **************************************************/
+void lcd_setbacklight(uint8_t r, uint8_t g, uint8_t b) {
+  // normalize the red LED - its brighter than the rest!
+  r = map(r, 0, 255, 0, 100);
+  g = map(g, 0, 255, 0, 150);
+  r = map(r, 0, 255, 0, lcd_brightness);
+  g = map(g, 0, 255, 0, lcd_brightness);
+  b = map(b, 0, 255, 0, lcd_brightness);
+  // common anode so invert!
+  r = map(r, 0, 255, 255, 0);
+  g = map(g, 0, 255, 255, 0);
+  b = map(b, 0, 255, 255, 0);
+  analogWrite(LCD_R, r);
+  analogWrite(LCD_G, g);
+  analogWrite(LCD_B, b);
+}
+
+/*************************************************
+ * Displaying a splash screen at startup
+ **************************************************/
+void lcd_splash_screen() {
+  // first line
+  lcd.setCursor(0, 0);
+  lcd.print(" BrewFlowMeter ");  
+  // second line
+  lcd.setCursor(0, 1);
+  lcd.print(" v");
+  lcd.print(APP_VERSION);
+  lcd.print(" by Pilooz ");
+
+  // background color Orange
+  for (int i = 0; i < 255; i++) {
+    lcd_setbacklight(i, 0, 255-i);
+    delay(5);
+  }
+  for (int i = 0; i < 255; i++) {
+    lcd_setbacklight(255-i, i, 0);
+    delay(5);
+  }
+  for (int i = 0; i < 255; i++) {
+    lcd_setbacklight(0, 255-i, i);
+    delay(5);
+  }
+}
+
+/*************************************************
+ * Setup for serial
+ **************************************************/
+void serial_setup() {
+  Serial.begin(9600);
+  debug("----- BrewFlowMeter Debug -----");
+}
 
 // --------------------------------------------------------
 // Setup
 // --------------------------------------------------------
 void setup() {
+  // LCD
+  // Setup backlight color.
+  pinMode(LCD_R, OUTPUT);
+  pinMode(LCD_G, OUTPUT);
+  pinMode(LCD_B, OUTPUT);
+  lcd.begin(16, 2);
+  lcd.clear();
+  // Slash screen
+  lcd_splash_screen();
+
   // Encoder settings
   pinMode(ENC_PUSH, INPUT); 
   digitalWrite(ENC_PUSH, HIGH);
@@ -60,16 +182,15 @@ void setup() {
   digitalWrite(ENC_B, HIGH);
   PCintPort::attachInterrupt(ENC_B, &doEncoderB, CHANGE);
   encoderPos = 0;
-  
+
   // Serial settings
-  if (debug_mode) {
-  Serial.begin(9600);
-  debug("----- BrewFlowMeter Debug -----");
-  }
+  //  if (debug_mode) {
+  serial_setup();
+  //  }
 }
 
 void debug(String s) {
-    Serial.println(s);
+  Serial.println(s);
 }
 
 // --------------------------------------------------------
@@ -78,13 +199,13 @@ void debug(String s) {
 void loop(){ 
   if (lastReportedPos != encoderPos) {
     switch (encoderPos - lastReportedPos) {
-      case -1:
-       app_target_liters = encoderPos * ENC_STEP;
-       break;
-      case 1:
-       app_target_liters = encoderPos * ENC_STEP;
-       break;
-      default:
+    case -1:
+      app_target_liters = encoderPos * ENC_STEP;
+      break;
+    case 1:
+      app_target_liters = encoderPos * ENC_STEP;
+      break;
+    default:
       break;
     } 
     Serial.print(app_target_liters);
@@ -118,11 +239,69 @@ void doEncoderB(){
   }
 }
 
-// Interrupt when button is pushed
+/*************************************************
+ * setting encoder event to pushed 
+ * (when encoder button is pushed)
+ * 
+ * This is the Interface Controller
+ * This is called on interrupt #1
+ **************************************************/
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
 void button_pushed() {
-  Serial.print("Button pushed "); 
-  Serial.println("!");
-  encoderPos = 0;
+  Serial.println("Button pushed !");
+  noInterrupts();
+  //  encoder_button_state = digitalRead(ENC_PUSH);
+  if( (millis() - lastDebounceTime) > debounceDelay){
+    //    encoder_button_state = RE_PUSHED;
+    int previous_screen = app_get_previous_state();
+
+    // See where we were before this event
+    switch (previous_screen) {
+    case APP_WAITING:   
+      // We were on Waiting state, so go to APP_OPTIONS
+      app_set_state(APP_OPTIONS);
+      break;
+
+    case APP_RUNNING:    
+      // We were in running mode, so close valve and go to APP_WAITING
+      app_set_state(APP_WAITING);
+      break;
+
+    case APP_SETTING:   
+      // we were in setting mode, so validate setting and go to APP_WAITING
+      app_set_state(APP_WAITING);
+      break;
+
+    case APP_OPTIONS:   
+      // We were in options mode, so see which option was choosen.
+      switch (app_choice) {
+      case CHOICE_CANCEL:
+        app_set_state(APP_WAITING); // Canceling any action, go to waiting state
+        break;
+      case CHOICE_RUNNING:
+        app_set_state(APP_RUNNING); // Go to running mode, valve opened
+        break;
+      case CHOICE_SETTING:
+        app_set_state(APP_SETTING);
+        break;
+      default:
+        break;
+      }
+      break;
+    default:
+      // see if we need to reset something ?
+      break;
+    } 
+    Serial.print("app_state=");
+    Serial.println(app_get_state());
+    Serial.print("encoder_button_state=");
+    Serial.println(encoder_button_state);
+  } 
+  interrupts();
 }
+
+
 
 
