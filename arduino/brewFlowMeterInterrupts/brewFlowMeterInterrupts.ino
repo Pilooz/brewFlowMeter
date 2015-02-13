d61bdd00c4b1d8f6a6cda7fdff0e50f97978e65f
Pierre-Gilles Levallois <pgl@erasme.org>
2015-02-13 14:38:40 +0100

    full functional version

--- arduino/brewFlowMeterInterrupts/brewFlowMeterInterrupts.ino
+++ arduino/brewFlowMeterInterrupts/brewFlowMeterInterrupts.ino
@@ -17,7 +17,10 @@
 
 #define APP_VERSION "1.0"
 
-// Rotary encoder push action on Pin #3 for interrupt 1
+// Liquid Flow sensor
+#define FLW 2
+
+// Rotary encoder push action
 #define ENC_PUSH 3 //Label SW on encoder 
 #define ENC_A  7  // Label DT on encoder
 #define ENC_B  8  // Label CLK on encoder
@@ -31,7 +34,11 @@
 #define VLV 13
 
 #define ENC_STEP  0.1
+
 // Application status
+#define APP_ERROR   -1000
+// App is displaying splash screen while initializing.
+#define APP_SPLASH -1
 // App is waiting for sensors or buttons changes : Valve is closed
 // displaying current passing volume, desired volume, total volume, flowrate
 // push button may open valve after APP_CONFIRM mode
@@ -68,6 +75,7 @@ volatile int encoderPos = 0;
 volatile int lastReportedPos = 1;
 boolean A_set = false;
 boolean B_set = false;
+// The bstate of the button 0 : rest, 1 pushed
 int encoder_button_state = 0;
 
 //    uint8_t latest_interrupted_pin;
@@ -79,12 +87,27 @@ int lcd_brightness = 100;
 // Valve variables 0 : closed, 1:Opened
 int vlv_status = LOW;
 
+// Liquid Flow meter variables
+// count how many flw_pulses!
+volatile uint16_t flw_pulses = 0;
+// debounce display
+volatile uint16_t flw_pulses_old = 0;
+// Total flw_pulses
+uint16_t flw_total_pulses = 0;
+// track the state of the pulse pin
+volatile uint8_t flw_last_pinstate;
+// you can try to keep time of how long it is between flw_pulses
+volatile uint32_t flw_last_ratetimer = 0;
+// and use that to calculate a flow rate
+volatile float flw_rate;
+
 float app_target_liters = 0;
 
 // Application variables
 int app_status = APP_WAITING;
 int app_previous_status = APP_WAITING;
 int app_choice = CHOICE_CANCEL;
+String app_error = "";
 
 /*************************************************
  * Closing solenoid Valve
@@ -108,6 +131,10 @@ void vlv_open() {
 void app_set_state(int s) {
   app_previous_status = app_get_state();
   app_status = s;
+  Serial.print("app_status=");
+  Serial.println(s);
+  Serial.print("app_previous_status=");
+  Serial.println(app_previous_status);
 }
 
 /*************************************************
@@ -122,6 +149,35 @@ int app_get_state() {
  **************************************************/
 int app_get_previous_state() {
   return app_previous_status;
+}
+
+/*************************************************
+ * volume calculations
+ **************************************************/
+float calculateLiters(uint16_t p) {
+  // Sensor Frequency (Hz) = 7.5 * Q (Liters/min)
+  // Liters = Q * time elapsed (seconds) / 60 (seconds/minute)
+  // Liters = (Frequency (flw_pulses/second) / 7.5) * time elapsed (seconds) / 60
+  // Liters = flw_pulses / (7.5 * 60)
+  // if a brass sensor use the following calculation
+  if (p > 0) {
+    float l = p;
+    l /= 8.1;
+    l -= 6;
+    l /= 60.0;
+    return l;
+  }
+  return 0;
+}
+
+/*************************************************
+ * adusting backlight color on lcd 
+ * dealing with pct of volume flown
+ **************************************************/
+void lcd_adjust_backlight(int pct) {
+  // mapping pct between 0 and 255
+  map(pct, 0, 100, 0, 255);
+  lcd_setbacklight(pct, 255-pct, 0);
 }
 
 /*************************************************
@@ -204,13 +260,103 @@ void lcd_options_mode() {
   app_choice = encoderPos;
 }
 
+/*************************************************
+ * Displaying data on lcd 
+ * Step : APP_SETTING
+ * Turning the rotary enc CW increments by 1.0 L
+ * turninc CCW decrements by 0.1 L
+ * displaying :
+ *   Target volume ?
+ *   0.0 L 
+ **************************************************/
+void lcd_setting_mode() {  
+  //if (lastReportedPos != encoderPos) {
+    switch (encoderPos - lastReportedPos) {
+    case -1:
+      app_target_liters = encoderPos * ENC_STEP;
+      break;
+    case 1:
+      app_target_liters = encoderPos * ENC_STEP;
+      break;
+    default:
+      break;
+    } 
+//  }
+  // background color Orange
+  lcd_setbacklight(255, 165, 0);
+  // first line
+  lcd.setCursor(0, 0);
+  lcd.print("Target volume ? "); 
+  // second line
+  lcd.setCursor(0, 1);
+  lcd.print(app_target_liters);
+  lcd.print(" L ");
+}
+
+/*************************************************
+ * Displaying data on lcd 
+ * Step : APP_WAITING
+ * displaying :
+ *  flowrate L/s     total volume L
+ *  percent flow %   current passed volume / desired volume 
+ **************************************************/
+void lcd_waiting_mode() {
+  float liters = calculateLiters(flw_pulses);
+  float total_liters = calculateLiters(flw_total_pulses);
+  float pct = 0;
+  if (app_target_liters > 0) {
+    pct = (int)(100 *liters / app_target_liters);
+  }
+  if (vlv_status == HIGH) {
+    // Set backlight to blue
+    lcd_adjust_backlight(pct);
+  }
+  // first line
+  lcd.setCursor(0, 0);
+  lcd.print(flw_rate);
+  lcd.print("Hz "); 
+  lcd.print(total_liters);
+  lcd.print(" L");
+  // second line
+  lcd.setCursor(0, 1);
+  lcd.print((int)pct);
+  lcd.print("% ");
+  lcd.print(liters);
+  lcd.print("/");
+  lcd.print(app_target_liters);
+  lcd.print(" L");
+}
+
+/*************************************************
+ * Displaying data on lcd 
+ * Step : APP_RUNNING
+ * displaying :
+ *  flowrate L/s     total volume L
+ *  percent flow %   current passed volume / desired volume 
+ * @TODO : rounding values for a pretty display 
+ **************************************************/
+void lcd_running_mode() {
+  // Same screen as waiting mode for the moment
+  lcd_waiting_mode();
+}
+
+/*************************************************
+ * Setup for serial
+ **************************************************/
+void lcd_message(String msg) {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("/!\\ /!\\ /!\\ /!\\ ");
+  lcd.setCursor(0, 1);
+  lcd.print(msg);
+}  
 
 /*************************************************
  * Setup for serial
  **************************************************/
 void serial_setup() {
   Serial.begin(9600);
-  debug("----- BrewFlowMeter Debug -----");
+  Serial.print("Flow sensor and rotary encoder test!");
 }
 
 // --------------------------------------------------------
@@ -231,10 +377,18 @@ void setup() {
   pinMode(VLV, OUTPUT);
   digitalWrite(VLV, LOW);
   vlv_status = 0;
+
+  // Flowmeter settings
+  // Liquid Flow meter
+  pinMode(FLW, INPUT);
+  digitalWrite(FLW, HIGH);
+  flw_last_pinstate = HIGH;
+  PCintPort::attachInterrupt(FLW, &flw_read, RISING);
+
   // Encoder settings
   pinMode(ENC_PUSH, INPUT); 
   digitalWrite(ENC_PUSH, HIGH);
-  PCintPort::attachInterrupt(ENC_PUSH, &button_pushed, RISING);  // add more attachInterrupt code as required
+  PCintPort::attachInterrupt(ENC_PUSH, &button_pushed, RISING);
   pinMode(ENC_A, INPUT); 
   digitalWrite(ENC_A, HIGH);
   PCintPort::attachInterrupt(ENC_A, &doEncoderA, CHANGE);
@@ -242,35 +396,62 @@ void setup() {
   digitalWrite(ENC_B, HIGH);
   PCintPort::attachInterrupt(ENC_B, &doEncoderB, CHANGE);
   encoderPos = 0;
+  lastReportedPos = 0;
+  encoder_button_state = 0;
 
-  // Serial settings
-  //  if (debug_mode) {
+  // Setting initial state for screen application  
+  //app_status = APP_WAITING;
+  app_previous_status = APP_SPLASH;
+  app_choice = CHOICE_CANCEL;
+
   serial_setup();
-  //  }
-}
-
-void debug(String s) {
-  Serial.println(s);
 }
 
 // --------------------------------------------------------
 // Main loop
 // --------------------------------------------------------
 void loop(){ 
-  if (lastReportedPos != encoderPos) {
-    switch (encoderPos - lastReportedPos) {
-    case -1:
-      app_target_liters = encoderPos * ENC_STEP;
+// If encoder has moved or has been pushed
+ if (encoder_button_state == 1 || (lastReportedPos != encoderPos)) {
+    lcd.clear();
+    switch (app_get_state()) {
+    case APP_WAITING: // App is waiting for sensors or buttons changes : Valve is closed
+      vlv_close();
+      // Background color is blue, dodgerBlue.
+      lcd_setbacklight(30, 144, 255);
+      // displaying current passing volume, desired volume, total volume, flowrate
+      lcd_waiting_mode();
+      // push button may open valve after APP_OPTIONS mode
       break;
-    case 1:
-      app_target_liters = encoderPos * ENC_STEP;
+    case APP_RUNNING: // App is running water thru valve : valve is opened
+      vlv_open();
+      // displaying current passing volume, desired volume, total volume, flowrate
+      lcd_running_mode();
+      // push button may interrupt to close valve and return to APP_WAITING mode
+      break;
+    case APP_SETTING: // App is in setting mode, valve is closed
+      vlv_close();
+      // displying 'setting mode' on first line and desired volume to adjust on second line
+      lcd_setting_mode();
+      // turing the rotary encoder adjust desired volume of water,
+      // push button may set adjusted volume of water and return to APP_WAITING mode
+      break;
+    case APP_OPTIONS: // App is in confirmation mode, valve is closed
+      vlv_close();
+      // displaying a confirmation message on first line and  run / set / cancel choice on second line.
+      lcd_options_mode();
+      // push button may set desired volume and return to APP_WAITING mode
       break;
     default:
+      // Close the valve by security
+      vlv_close();
+      // see if we need to reset something ? treating with errors
+      lcd_message(app_error);
+      app_set_state(APP_ERROR);
       break;
-    } 
-    Serial.print(app_target_liters);
-    Serial.println("L");
+    }
     lastReportedPos = encoderPos;
+    encoder_button_state = 0; 
   }
 }
 
@@ -310,32 +491,30 @@ long lastDebounceTime = 0;  // the last time the output pin was toggled
 long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
 void button_pushed() {
-  Serial.println("Button pushed !");
   noInterrupts();
-  //  encoder_button_state = digitalRead(ENC_PUSH);
-  if( (millis() - lastDebounceTime) > debounceDelay){
-    //    encoder_button_state = RE_PUSHED;
+  if( ((long)millis() - lastDebounceTime) > debounceDelay){
+    // Marking the utton as pushed
+    encoder_button_state = 1;
     int previous_screen = app_get_previous_state();
-
     // See where we were before this event
     switch (previous_screen) {
+    case APP_SPLASH:   
+      // We were on Waiting state, so go to APP_OPTIONS
+      app_set_state(APP_WAITING);
+      break;
+
     case APP_WAITING:   
       // We were on Waiting state, so go to APP_OPTIONS
-      vlv_close();
       app_set_state(APP_OPTIONS);
-      lcd_options_mode();
       break;
 
     case APP_RUNNING:    
       // We were in running mode, so close valve and go to APP_WAITING
-      vlv_close();
       app_set_state(APP_WAITING);
-      //      lcd_waiting_mode();
       break;
 
     case APP_SETTING:   
       // we were in setting mode, so validate setting and go to APP_WAITING
-      vlv_close();
       app_set_state(APP_WAITING);
       break;
 
@@ -346,7 +525,6 @@ void button_pushed() {
         app_set_state(APP_WAITING); // Canceling any action, go to waiting state
         break;
       case CHOICE_RUNNING:
-        vlv_open();
         app_set_state(APP_RUNNING); // Go to running mode, valve opened
         break;
       case CHOICE_SETTING:
@@ -358,34 +536,33 @@ void button_pushed() {
       break;
     default:
       // see if we need to reset something ?
+      app_error = "No known state for LCD ?";
+      app_set_state(APP_ERROR);
       break;
     } 
-
-    Serial.print("app_state=");
-    switch (app_get_state()) {
-    case APP_WAITING: 
-      Serial.print("APP_WAITING"); 
-      break;
-    case APP_RUNNING: 
-      Serial.print("APP_RUNNING"); 
-      break;
-    case APP_SETTING: 
-      Serial.print("APP_SETTING"); 
-      break;
-    case APP_OPTIONS: 
-      Serial.print("APP_OPTIONS"); 
-      break;
-    default: 
-      Serial.print("Non persistent state ?"); 
-      break;
-    }
-    Serial.println();
-    Serial.print("valve=");
-    Serial.println(vlv_status);
   } 
   interrupts();
 }
 
+// --------------------------------------------------------
+// Interrupts for flowmeter
+// --------------------------------------------------------
+void flw_read() {
+  uint8_t x = digitalRead(FLW);
+  if (x == flw_last_pinstate) {
+    flw_last_ratetimer++;
+    return; // nothing changed!
+  }
+  if (x == HIGH) {
+    //low to high transition!
+    flw_pulses++;
+  }
+  flw_last_pinstate = x;
+  flw_rate = 1000.0;
+  flw_rate /= flw_last_ratetimer; // in hertz
+  flw_last_ratetimer = 0;
+  flw_total_pulses = flw_pulses;
+}
 
 
 
